bb444930ee6106471ad04bfdc28399a7f911ee61
lucas <luk.2001@hotmail.com>
2023-01-21 20:14:45 +0000

    Adiciona PID e alterações na main

--- main.py
+++ main.py
@@ -1,50 +1,119 @@
 from connections.Uart import Uart
 import connections.Modbus as Modbus
+from Utils.Pid import PID
 import struct
 from connections.I2C import I2C
 from Forno import Forno
 import time
+from threading import Event, Thread 
 
-uart = Uart()
+class Main():
+    uart = Uart()
+    pid = PID()
+    forno = Forno()
+    ref_temp = 0
+    internal_temp = 0
+    response = 0
 
-uart.write(Modbus.request_internal_temp,  7)
+    def __init__(self):
+        self.menu()
 
-data = uart.read()
-   
-temp = struct.unpack('f', data)[0]
-print(temp)
+    def menu(self):
+        while(True):
 
-forno = Forno()
+            self.read_user_comands()
 
-while(True):
-    uart.write(Modbus.read_user_commands,  7)
+            print(self.response)
 
-    response = uart.read()
-    response = struct.unpack('i', response)[0]
+            if self.response == 161:
+                message = Modbus.send_sys_on_off + b'\x01'
+                self.uart.write(message,  8)
+                data = self.uart.read()
+                if data == b'\x01\x00\x00\x00':
+                    print("Liga o Forno")
 
-    if response == 161:
-        message = Modbus.send_sys_on_off + b'\x01'
-        uart.write(message,  8)
-        data = uart.read()
-        if data == b'\x01\x00\x00\x00':
-            print("Sistema Ligado")
-
-    elif response == 162:
-        message = Modbus.send_sys_on_off + b'\x00'
-        uart.write(message,  8)
-        data = uart.read()
-        if data == 0:
-            print("Sistema Desligado")
-            
-    elif response == 163:
-        forno.heat()
-    elif response == 164:
-        print("FunFoou")
-        pass
-    elif response == 165:
-        pass
-    else:
-        pass 
-
-    time.sleep(0.5)
+            elif self.response == 162:
+                message = Modbus.send_sys_on_off + b'\x00'
+                self.uart.write(message,  8)
+                data = self.uart.read()
         
+                if data == b'\x00\x00\x00\x00':
+                    print("Desliga o Forno")
+                    
+            elif self.response == 163:
+                self.read_temperatures()
+
+                while self.internal_temp != self.ref_temp:
+                    pid_atual = self.pid.pid_controle(self.ref_temp, self.internal_temp)
+                    print("pid " + str(pid_atual))
+                    
+                    if(pid_atual < 0):
+                        pid_atual *= -1
+                        if(pid_atual < 40):
+                            pid_atual = 40
+                        self.forno.cool_down(pid_result)
+                    else:    
+                        self.forno.heat(self.pid.pid_controle(self.ref_temp, self.internal_temp))
+
+                    self.read_temperatures()
+                    self.read_user_comands()
+                    
+            elif self.response == 164:
+                room_temp = I2C()
+
+                self.uart.write(Modbus.request_internal_temp,  7)
+
+                data = self.uart.read()
+        
+                internal_temp = struct.unpack('f', data)[0]
+                pid_result = self.pid.pid_controle(room_temp, internal_temp)
+
+                pid_result *= -1
+                if pid_result < 40:
+                    pid_result = 40
+
+                self.forno.cool_down(pid_result)
+                pass
+            elif self.response == 165:
+                pass
+            else:
+                pass 
+
+
+    def read_temperatures(self):
+        self.read_ref_temp()
+        time.sleep(0.5)
+        self.read_internal_temp()
+        time.sleep(0.5)
+
+    
+    def read_ref_temp(self):
+        message = Modbus.request_ref_temp
+
+        self.uart.write(message,  7)
+        data = self.uart.read()
+        self.ref_temp = struct.unpack('f', data)[0]
+
+        print(self.ref_temp)
+
+    
+    def read_internal_temp(self):
+        message = Modbus.request_internal_temp
+
+        self.uart.write(message,  7)
+        data = self.uart.read()
+        self.internal_temp = struct.unpack('f', data)[0]
+
+        print(self.internal_temp)
+    
+    def read_user_comands(self):
+            print("Esperando comando...")
+            self.uart.write(Modbus.read_user_commands,  7)
+
+            response = self.uart.read()
+            self.response = struct.unpack('i', response)[0]
+            time.sleep(0.5)
+
+
+if __name__ == '__main__':
+    Main()
